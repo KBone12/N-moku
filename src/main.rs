bacd3be1ba20671cca1f7fc6f2c6960bf5dc1b10
kbone <kbonehobby@gmail.com>
2022-07-09 21:04:41 +0900

    Put a stone

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,92 @@ use crossterm::{
     terminal::{Clear, ClearType, EnterAlternateScreen, LeaveAlternateScreen},
 };
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Eq, PartialEq)]
 enum Stone {
     Empty,
+    Black,
+    White,
+}
+
+struct Board {
+    stones: Vec<Vec<Stone>>,
+}
+
+impl Board {
+    pub fn new(n: usize) -> Self {
+        Self {
+            stones: vec![vec![Stone::Empty; n]; n],
+        }
+    }
+
+    pub const fn stones(&self) -> &Vec<Vec<Stone>> {
+        &self.stones
+    }
+
+    pub fn is_empty(&self, x: usize, y: usize) -> bool {
+        self.stones[y][x] == Stone::Empty
+    }
+
+    pub fn put(&mut self, stone: Stone, x: usize, y: usize) {
+        self.stones[y][x] = stone;
+    }
+}
+
+struct Renderer<W: Write> {
+    writer: W,
+    n: usize,
+    offset_x: u16,
+    offset_y: u16,
+}
+
+impl<W: Write> Renderer<W> {
+    pub fn new(writer: W, n: usize) -> Self {
+        let (width, height) =
+            crossterm::terminal::size().expect("Can't get the size of the screen");
+        let offset_x = ((width as usize - n) / 2) as _;
+        let offset_y = ((height as usize - n) / 2) as _;
+
+        Self {
+            writer,
+            n,
+            offset_x,
+            offset_y,
+        }
+    }
+
+    pub fn render_board(&mut self, board: &Board) {
+        crossterm::queue!(self.writer, MoveTo(self.offset_x, self.offset_y))
+            .expect("Can't enqueue a command for moving the cursors");
+        board.stones().iter().for_each(|row| {
+            write!(
+                self.writer,
+                "{}",
+                row.iter()
+                    .map(|stone| match stone {
+                        Stone::Empty => '-',
+                        Stone::Black => 'o',
+                        Stone::White => 'x',
+                    })
+                    .collect::<String>()
+            )
+            .expect("Can't enqueue commands for drawing a board");
+            crossterm::queue!(self.writer, MoveToNextLine(1), MoveToColumn(self.offset_x),)
+                .expect("Can't enqueue commands for moving cursors");
+        });
+        crossterm::queue!(self.writer, MoveTo(self.offset_x, self.offset_y))
+            .expect("Can't enqueue a command for moving the cursor");
+        self.writer.flush().expect("Can't flush commands");
+    }
+
+    pub fn process_event(&mut self, event: &Event) {
+        match event {
+            Event::Resize(width, height) => {
+                self.offset_x = ((*width as usize - self.n) / 2) as _;
+                self.offset_y = ((*height as usize - self.n) / 2) as _;
+            }
+            _ => {}
+        }
+    }
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -17,41 +100,34 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     crossterm::terminal::enable_raw_mode()?;
     crossterm::execute!(std::io::stdout(), Clear(ClearType::All))?;
 
+    let n = 3;
+    let mut board = Board::new(n);
+    let mut renderer = Renderer::new(std::io::stdout(), n);
+    let mut black_turn = true;
     loop {
-        let (screen_width, screen_height) = crossterm::terminal::size()?;
-        let screen_width = screen_width as usize;
-        let screen_height = screen_height as usize;
-        let n = 3;
-        let offset_x = (screen_width - n) / 2;
-        let offset_y = (screen_height - n) / 2;
-        let board = vec![vec![Stone::Empty; n]; n];
-        crossterm::queue!(std::io::stdout(), MoveTo(offset_x as _, offset_y as _))?;
-        board
-            .iter()
-            .map(|row| {
-                write!(
-                    std::io::stdout(),
-                    "{}",
-                    row.iter()
-                        .map(|stone| match stone {
-                            Stone::Empty => '-',
-                        })
-                        .collect::<String>()
-                )?;
-                crossterm::queue!(
-                    std::io::stdout(),
-                    MoveToNextLine(1),
-                    MoveToColumn(offset_x as _),
-                )
-            })
-            .collect::<Result<_, _>>()?;
-        crossterm::queue!(std::io::stdout(), MoveTo(offset_x as _, offset_y as _))?;
-        std::io::stdout().flush()?;
+        renderer.render_board(&board);
 
-        match crossterm::event::read()? {
+        let event = crossterm::event::read()?;
+        renderer.process_event(&event);
+        match event {
             Event::Key(KeyEvent {
                 code: KeyCode::Esc, ..
             }) => break,
+            Event::Key(KeyEvent {
+                code: KeyCode::Char(' '),
+                ..
+            }) if board.is_empty(0, 0) => {
+                board.put(
+                    if black_turn {
+                        Stone::Black
+                    } else {
+                        Stone::White
+                    },
+                    0,
+                    0,
+                );
+                black_turn = !black_turn;
+            }
             _ => {}
         }
     }
